d7fbe5c5d01c98a67cfed67073481147dcbb8a19
AbdulRehman <malik.abdul@bazaartech.com>
2024-06-21 15:44:55 +0500

    improved design

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['www.hiveart.xyz'],
+      },
+};
 
 export default nextConfig;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,67 @@
 import Image from "next/image";
+import { Syne } from "next/font/google";
+import "@/../styles/page.scss";
 
+const syne = Syne({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          src="/abdulrehman.jpeg"
-          height={240}
-          width={240}
-          alt="Display Picture"
-        ></Image>
+    <main className="flex min-h-screen flex-col  p-24">
+      <div className="flex flex-row gap-3">
+        <div className="w-3/5">
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="65"
+              height="2"
+              viewBox="0 0 65 2"
+              fill="none"
+              className="mt-2"
+            >
+              <path d="M0 1H65" stroke="#080808"></path>
+            </svg>
+            <span
+              className={`ml-2 ${syne.className}`}
+              style={{ fontSize: "32px" }}
+            >
+              Hello, I'm{" "}
+            </span>
+            <span>
+              <Image
+                className="mx-2"
+                src="https://www.hiveart.xyz/perez/assets/images/icon/victory.png"
+                alt="Hand Image"
+                width={20}
+                height={20}
+              />
+            </span>
+          </div>
+          <h1
+            style={{
+              fontSize: "80px",
+            }}
+            className={syne.className}
+          >
+            Malik Abdul Rehman Khursheed
+          </h1>
+        </div>
+
+        <div className="w-2/5">
+          <Image
+            src="/abdulrehman.jpeg"
+            height={770}
+            width={450}
+            alt="Display Picture"
+          />
+        </div>
       </div>
-      <h1>Malik Abdul Rehman Khursheed</h1>
-      <h1>My projects:</h1>
-      <div>bazaartech.com</div>
-      <div>bazaarpartnerportal.com</div>
-      <div>orcalo.com</div>
+
+      <h1 className={syne.className} style={{fontSize:"40px"}}>My projects:</h1>
+      <div className="projects">bazaartech.com</div>
+      <div className="projects">bazaarpartnerportal.com</div>
+      <div className="projects">orcalo.com</div>
     </main>
   );
 }
