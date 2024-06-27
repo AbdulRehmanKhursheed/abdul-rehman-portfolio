8cb1fff2595a6bb80ab8f51a98213482108cc891
AbdulRehman <malik.abdul@bazaartech.com>
2024-06-27 22:44:44 +0500

    commented for a while

--- components/name.component.tsx
+++ components/name.component.tsx
@@ -10,7 +10,8 @@ const NameComponent = () => {
     <h1
       className={`${syne.className} py-6 text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-6xl leading-tight`}
     >
-      Malik Abdul Rehman Khursheed
+      {/* Malik Abdul Rehman Khursheed */}
+      {"Testing Deep link :)"}
     </h1>
   );
 };
