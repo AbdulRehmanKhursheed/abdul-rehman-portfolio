0920f753513a1698fc561dcc2001398293b5a7b3
AbdulRehman <malik.abdul@bazaartech.com>
2024-06-28 01:45:29 +0500

    changed few things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+
+'use client'
+
+
 import "@/../styles/page.scss";
 import DisplayPictureComponent from "../../components/display-picture.component";
 import NameComponent from "../../components/name.component";
@@ -5,9 +9,25 @@ import GreetingsComponent from "../../components/greetings.component";
 import ProfessionalTagLineComponent from "../../components/professional-tagline.component";
 import ProjectSection from "../../components/projects-section.component";
 
-
+import { useEffect } from 'react';
+import { NextPage } from 'next';
 
 export default function Home() {
+  // pages/index.tsx
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const userAgent = navigator.userAgent || navigator.vendor;
+      const androidAppLink = 'intent://abdul-rehman-portfolio-ecru.vercel.app/#Intent;scheme=https;package=bazaar.tech.com;end';
+
+      // Check if the user is on an Android device
+      if (/android/i.test(userAgent)) {
+        console.log("Android device detected, redirecting to app...");
+        window.location.href = androidAppLink;
+      }
+    }
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col">
       <div
