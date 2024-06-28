516b11ffdddd85a218a64c17db2233fb902dedf6
AbdulRehman <malik.abdul@bazaartech.com>
2024-06-28 11:10:05 +0500

    commented the redirection code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 import "@/../styles/page.scss";
 import DisplayPictureComponent from "../../components/display-picture.component";
@@ -9,26 +9,26 @@ import ProjectSection from "../../components/projects-section.component";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const packageName = "com.your.android.app.package";
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const packageName = "com.your.android.app.package";
 
-      const urlString = window.location.href;
+  //     const urlString = window.location.href;
 
-      const urlParams = new URLSearchParams(urlString);
+  //     const urlParams = new URLSearchParams(urlString);
 
-      const userAgent = navigator.userAgent || navigator.vendor;
-      const referralCode = urlParams.get("referralCode");
+  //     const userAgent = navigator.userAgent || navigator.vendor;
+  //     const referralCode = urlParams.get("referralCode");
 
-      const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app?referralCode=${referralCode}/#Intent;scheme=https;package=bazaar.tech.com;end`;
+  //     const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app?referralCode=${referralCode}/#Intent;scheme=https;package=bazaar.tech.com;end`;
 
-      // Check if the user is on an Android device
-      if (/android/i.test(userAgent)) {
-        console.log("Android device detected, redirecting to app...");
-        window.location.href = androidAppLink;
-      }
-    }
-  }, []);
+  //     // Check if the user is on an Android device
+  //     if (/android/i.test(userAgent)) {
+  //       console.log("Android device detected, redirecting to app...");
+  //       window.location.href = androidAppLink;
+  //     }
+  //   }
+  // }, []);
 
   return (
     <main className="flex min-h-screen flex-col">
