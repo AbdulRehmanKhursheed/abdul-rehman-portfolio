d72bd0148c20c3935522f7102df26a2711929cdf
AbdulRehman <malik.abdul@bazaartech.com>
2024-06-28 12:23:20 +0500

    added a new route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,11 @@
-"use client";
-
 import "@/../styles/page.scss";
 import DisplayPictureComponent from "../../components/display-picture.component";
 import NameComponent from "../../components/name.component";
 import GreetingsComponent from "../../components/greetings.component";
 import ProfessionalTagLineComponent from "../../components/professional-tagline.component";
 import ProjectSection from "../../components/projects-section.component";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const packageName = "com.your.android.app.package";
-
-      const urlString = window.location.href;
-
-      const urlParams = new URLSearchParams(urlString);
-
-      const userAgent = navigator.userAgent || navigator.vendor;
-      const referralCode = urlParams.get("referralCode");
-
-      // const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app?referralCode=${referralCode}/#Intent;scheme=https;package=bazaar.tech.com;end`;
-      const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app/referralCode=${referralCode}#Intent;scheme=https;package=bazaar.tech.com;S.browser_fallback_url=https://play.google.com/store/apps/details?id=bazaar.tech.com;end`;
-
-      // Check if the user is on an Android device
-      if (/android/i.test(userAgent)) {
-        console.log("Android device detected, redirecting to app...");
-        window.location.href = androidAppLink;
-      }
-    }
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col">
       <div
---
+++ src/app/referral/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+
+const Referral=()=>{
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+          const packageName = "com.your.android.app.package";
+    
+          const urlString = window.location.href;
+    
+          const urlParams = new URLSearchParams(urlString);
+    
+          const userAgent = navigator.userAgent || navigator.vendor;
+          const referralCode = urlParams.get("referralCode");
+    
+          // const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app?referralCode=${referralCode}/#Intent;scheme=https;package=bazaar.tech.com;end`;
+          const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app/referralCode=${referralCode}#Intent;scheme=https;package=bazaar.tech.com;S.browser_fallback_url=https://play.google.com/store/apps/details?id=bazaar.tech.com;end`;
+    
+          // Check if the user is on an Android device
+          if (/android/i.test(userAgent)) {
+            console.log("Android device detected, redirecting to app...");
+            window.location.href = androidAppLink;
+          }
+        }
+      }, []);
+
+    return <p>Referral Page</p>
+}
+export default Referral;
