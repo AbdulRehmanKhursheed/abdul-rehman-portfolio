e5996dd7168b5e35956b00fccb9a48e0ad917335
AbdulRehman <malik.abdul@bazaartech.com>
2024-06-28 14:28:45 +0500

    redirecting..

--- src/app/referral/page.tsx
+++ src/app/referral/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import { useEffect } from "react";
+import { Syne } from "next/font/google";
+
+const syne = Syne({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 const Referral = () => {
   useEffect(() => {
@@ -13,10 +19,11 @@ const Referral = () => {
 
       const userAgent = navigator.userAgent || navigator.vendor;
       const referralCode = urlParams.get("referralCode");
+      console.log(referralCode);
 
       // const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app?referralCode=${referralCode}/#Intent;scheme=https;package=bazaar.tech.com;end`;
       const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app/referralCode=${referralCode}#Intent;scheme=https;package=bazaar.tech.com;S.browser_fallback_url=https://play.google.com/store/apps/details?id=bazaar.tech.com;end`;
-
+      console.log(androidAppLink);
       // Check if the user is on an Android device
       if (/android/i.test(userAgent)) {
         console.log("Android device detected, redirecting to app...");
@@ -25,6 +32,10 @@ const Referral = () => {
     }
   }, []);
 
-  return <p>Refresh the page if it doesn't take you to the Bazaar App.</p>;
+  return (
+    <div className={`m-5 ${syne.className}`}>
+      <p>Refresh the page if it doesn&apos;t take you to the Bazaar App.</p>
+    </div>
+  );
 };
 export default Referral;
