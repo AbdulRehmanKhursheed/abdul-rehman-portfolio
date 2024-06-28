1a4abeb65df1d3a234f392eb89abb0685f8b7b19
AbdulRehman <malik.abdul@bazaartech.com>
2024-06-28 13:22:59 +0500

    asdasd

--- src/app/referral/page.tsx
+++ src/app/referral/page.tsx
@@ -2,29 +2,29 @@
 
 import { useEffect } from "react";
 
-const Referral=()=>{
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-          const packageName = "com.your.android.app.package";
-    
-          const urlString = window.location.href;
-    
-          const urlParams = new URLSearchParams(urlString);
-    
-          const userAgent = navigator.userAgent || navigator.vendor;
-          const referralCode = urlParams.get("referralCode");
-    
-          // const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app?referralCode=${referralCode}/#Intent;scheme=https;package=bazaar.tech.com;end`;
-          const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app/referralCode=${referralCode}#Intent;scheme=https;package=bazaar.tech.com;S.browser_fallback_url=https://play.google.com/store/apps/details?id=bazaar.tech.com;end`;
-    
-          // Check if the user is on an Android device
-          if (/android/i.test(userAgent)) {
-            console.log("Android device detected, redirecting to app...");
-            window.location.href = androidAppLink;
-          }
-        }
-      }, []);
+const Referral = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const packageName = "com.your.android.app.package";
 
-    return <p>Referral Page</p>
-}
+      const urlString = window.location.search;
+
+      const urlParams = new URLSearchParams(urlString);
+
+      const userAgent = navigator.userAgent || navigator.vendor;
+      const referralCode = urlParams.get("referralCode");
+
+      // const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app?referralCode=${referralCode}/#Intent;scheme=https;package=bazaar.tech.com;end`;
+      const androidAppLink = `intent://abdul-rehman-portfolio-ecru.vercel.app/referralCode=${referralCode}#Intent;scheme=https;package=bazaar.tech.com;S.browser_fallback_url=https://play.google.com/store/apps/details?id=bazaar.tech.com;end`;
+
+      // Check if the user is on an Android device
+      if (/android/i.test(userAgent)) {
+        console.log("Android device detected, redirecting to app...");
+        window.location.href = androidAppLink;
+      }
+    }
+  }, []);
+
+  return <p>Referral Page</p>;
+};
 export default Referral;
