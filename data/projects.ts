cb460bcbba44bfcd2b32aec34e36a7c79cda88e6
AbdulRehman <malik.abdul@bazaartech.com>
2025-04-19 00:17:16 +0500

    updated projects

--- data/projects.ts
+++ data/projects.ts
@@ -1,52 +1,39 @@
 const projects = [
   {
+    projectName: "Bazaar Customer Web App",
+    description:
+      "Built a React/Next.js front-end (with MUI, SSR, and SWR caching) for an end-to-end B2B & B2C shopping experience. Features include product catalog, cart, checkout, payment gateway integration, and user profile management with Test-Driven Development (Jest/RTL) achieving 99% coverage for front-end and 100% for backend.",
+    link: "https://www.bazaarapp.com",
+  },
+  {
     projectName: "MYSITE main website",
     description:
       "MYSITE is a platform that simplifies reality capture and collaboration for construction projects, enabling users to document sites visually, manage projects remotely, and resolve disputes efficiently.",
     link: "https://mysitetech.com/",
   },
   {
-    projectName: "Bazaar Web App",
-    description:
-      " The Bazaar Customer Web App is basically web version of android bazaar app which is designed to provide a comprehensive and intuitive shopping experience for Bazaar customers (B2B & B2C both now). This application allows users to browse products, make purchases, and manage their orders efficiently.",
-    link: "https://www.bazaarapp.com/",
-  },
-
-  {
     projectName: "Bazaar Main Website",
     description:
-      "Though this app is built using WebFlow, I Integrated dynamic job postings in careers page using HTML, CSS, and Vanilla JavaScript.",
+      "Though this app is built using WebFlow, I Integrated fully dynamic job postings in careers page using HTML, CSS, and Vanilla JavaScript.",
     link: "https://bazaartech.com",
   },
   {
     projectName: "Bazaar Partner Portal",
     description:
-      "Built a React web app portal for Bazaar partners from scratch. Provided front-end development expertise and product insights, empowering the team to understand Bazaar’s offerings and areas for them to contribute by implementing Test Driven Development and Micro Front-ends (Module Federation).",
+      "Built a React web app portal for Bazaar partners from scratch. Provided front-end development expertise and product insights, empowering the team to understand Bazaar's offerings and areas for them to contribute by implementing Test Driven Development and Micro Front-ends (Module Federation) with Test-Driven Development (Jest/RTL) achieving 90%+ coverage.",
     link: "https://partners.bazaar.technology/",
   },
   {
-    projectName: "RND Tax Claim",
+    projectName: "Commando - bazaar internal tool.",
     description:
-      "RND Tax Claims provides a solution to improve the way organizations prepare and submit claims to HMRC for R&D tax relief. Our specialized tax software makes it easy for businesses to prepare clear and concise claims, either directly or through a business advisor. \n Whether you are an existing customer of or service provider to RND Tax Claims, or you just want to get in touch with us to find out more information about us and our solution, you can trust that we will respect your privacy and are committed to protecting your personal data.",
-    link: "https://app.rndtaxclaims.uk/signIn",
-  },
-  {
-    projectName: "Check my dbs ",
-    description:
-      "Check my dbs is basically a UK project where if employe apply for a job in any company in Uk than company require the DBS certificate of that person from the Uk government. DBS certificate tell the company about the person wheater he/she has any criminal record or not. DBS also maintains the adults' and children's Barred List and makes considered decisions as to whether an individual should be included on one or both of these lists and barred from engaging in regulated activity. • Incorporate a comprehensive authentication module into this project.",
-    link: "https://app-dev.checkmydbs.co.uk/login",
+      "It is an internal tool used by Bazaar to track and manage orders, inventory, riders, bazaar agents and many other internal tasks.",
+    link: "https://platform.bazaar.technology/login",
   },
   {
     projectName: "Student Help Squad",
     description:
       "Student Help Squad offers flexible internship opportunities to students within UK. Our internship program will accommodate both graduates willing to work with us at office and international graduates who want to work from home. We will help you enhance your technical and professional skills with our experienced professionals who have subject matter expertise in respective areas with hands-on industry work experience. Our internship program is career-oriented which will help you secure a good job based on the knowledge and skills you will gain with us. Industry-relevant skills, Working with others (teamwork, interpersonal, and communication), Self-management (organisational skillsplanning, and time management), Commercial awareness, Personal and professional development",
     link: "https://studenthelpsquad.co.uk/",
-  },
-  {
-    projectName: "Orcalo Holdings (now Outfit Consultancy) Main Website",
-    description:
-      "Orcalo is a UK based Company that has an offshore Company registered as Oufit Consultancy.",
-    link: "https://app-dev.mortgagebasket.co.uk/dashboard",
   },
 ];
 
