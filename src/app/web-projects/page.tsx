a52880ba0e819dcdfe79b617118bfbb7d1438a51
AbdulRehman <malik.abdul@bazaartech.com>
2025-04-19 01:26:49 +0500

    updated

--- src/app/web-projects/page.tsx
+++ src/app/web-projects/page.tsx
@@ -1,4 +1,6 @@
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 import ProjectElementComponent from "../../../components/project-element.component";
 import projects from "../../../data/projects";
