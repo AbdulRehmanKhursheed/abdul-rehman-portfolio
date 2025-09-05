e53fda235bbbcaed83d4ffab40396b66adf8621b
Abdul Rehman <malik.abdul@bazaartech.com>
2025-09-05 14:24:50 +0500

    footer title fix

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -168,7 +168,7 @@ const Footer = () => {
           >
             <div className="flex flex-col md:flex-row items-center  space-y-4 md:space-y-0">
               <div className="flex items-center space-x-2 text-slate-400">
-                <span>Designed & Built with</span>
+                <span>Built with</span>
                 <Heart className="w-4 h-4 text-red-500 fill-current animate-pulse" />
                 <span>by Abdul Rehman</span>
               </div>
